f102b6ed067912e1611746b27ef8470f617ef246
Tobias <tobiasjpark73@gmail.com>
2020-02-28 16:41:33 -0500

    Created test file

--- first-order/test.cpp
+++ first-order/test.cpp
@@ -7,7 +7,65 @@
 
 
 int main(int argc, char* argv[]){
-    FirstOrderTree andOrs = new FirstOrderTree("(A) & (B)");
-    andOrs.print();
-    andOrs.printTree();
+    FirstOrderTree andExpr("(A) & (B)");
+    andExpr.print();
+    std::cout << "\n\n";
+    andExpr.printTree();
+    std::cout << "\n\n";
+    std::cout << "\n\n";
+
+    FirstOrderTree orExpr("(A) | (B)");
+    orExpr.print();
+    std::cout << "\n\n";
+    orExpr.printTree();
+    std::cout << "\n\n";
+    std::cout << "\n\n";
+
+    FirstOrderTree notExpr("~(A)");
+    notExpr.print();
+    std::cout << "\n\n";
+    notExpr.printTree();
+    std::cout << "\n\n";
+    std::cout << "\n\n";
+
+    FirstOrderTree andOrExpr("((A) & (B)) | (C)");
+    andOrExpr.print();
+    std::cout << "\n\n";
+    andOrExpr.printTree();
+    std::cout << "\n\n";
+    std::cout << "\n\n";
+
+    FirstOrderTree andOrNotExpr("~(((A) & (B)) | (C))");
+    andOrNotExpr.print();
+    std::cout << "\n\n";
+    andOrNotExpr.printTree();
+    std::cout << "\n\n";
+    std::cout << "\n\n";
+
+    FirstOrderTree implicExpr("(A) > (B)");
+    implicExpr.print();
+    std::cout << "\n\n";
+    implicExpr.printTree();
+    std::cout << "\n\n";
+    std::cout << "\n\n";
+
+    FirstOrderTree advImplicExpr("((A) & (B)) > (C)");
+    advImplicExpr.print();
+    std::cout << "\n\n";
+    advImplicExpr.printTree();
+    std::cout << "\n\n";
+    std::cout << "\n\n";
+
+    FirstOrderTree biconExpr("(A) = (B)");
+    biconExpr.print();
+    std::cout << "\n\n";
+    biconExpr.printTree();
+    std::cout << "\n\n";
+    std::cout << "\n\n";
+
+    FirstOrderTree advBiconExpr("((A) & (B)) = (C)");
+    advBiconExpr.print();
+    std::cout << "\n\n";
+    advBiconExpr.printTree();
+
 }
