a605c65d986593cf09f47eb709247c43ab0de474
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-04-21 19:26:57 -0400

    Updated Testing Suite For Proof Checking.

--- first-order/ProofCheckerTest.cpp
+++ first-order/ProofCheckerTest.cpp
@@ -5,11 +5,13 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    // Test AND Intro
+    //Test Cases For AND Introduction Rule:
+    cout << "Test Suite For AND Introduction Rule: " << endl;
     ProofChecker expression = ProofChecker("A & B");
     expression.addChild(FirstOrderTree("A"));
     expression.addChild(FirstOrderTree("B"));
     expression.changeJustification("&I");
+    cout << "Expected Output: 1" << endl;
     std::cout << "Result of AND Introduction Rule Test #1: " << expression.isValid() << endl;
     cout << endl;
 
@@ -17,21 +19,46 @@ int main(int argc, char* argv[]){
     expression.addChild(FirstOrderTree("A"));
     expression.addChild(FirstOrderTree("B"));
     expression.changeJustification("&I");
+    cout << "Expected Output: 0" << endl;
     std::cout << "Result of AND Introduction Rule Test #2: " << expression.isValid() << endl;
+    cout << endl;
+
+    expression = ProofChecker("~A & ~B");
+    expression.addChild(FirstOrderTree("~A"));
+    expression.addChild(FirstOrderTree("~B"));
+    expression.changeJustification("&I");
+    cout << "Expected Output: 1" << endl;
+    std::cout << "Result of AND Introduction Rule Test #3: " << expression.isValid() << endl;
+    cout << endl;
+
+    expression = ProofChecker("~A & ~A");
+    expression.addChild(FirstOrderTree("~A"));
+    expression.addChild(FirstOrderTree("~A"));
+    expression.changeJustification("&I");
+    cout << "Expected Output: 1" << endl;
+    std::cout << "Result of AND Introduction Rule Test #4: " << expression.isValid() << endl;
+    cout << endl;
+
+    expression = ProofChecker("~A & A");
+    expression.addChild(FirstOrderTree("~A"));
+    expression.addChild(FirstOrderTree("A"));
+    expression.changeJustification("&I");
+    cout << "Expected Output: 1" << endl;
+    std::cout << "Result of AND Introduction Rule Test #5: " << expression.isValid() << endl;
     cout << endl;
 
     // Test AND Elim
     expression = ProofChecker("A");
     expression.addChild(FirstOrderTree("A & B"));
     expression.changeJustification("&E");
-    std::cout << "Result of AND Elim (A & B) => A: " << expression.isValid() << endl;
+    std::cout << "Result of AND Eliminiation Rule Test #1: " << expression.isValid() << endl;
     cout << endl;
 
     // Test OR Intro
     expression = ProofChecker("A | B");
     expression.addChild(FirstOrderTree("A"));
     expression.changeJustification("|I");
-    std::cout << "Result of OR Intro (A) => A | B: " << expression.isValid() << endl;
+    std::cout << "Result of OR Introduction Rule Test #1: " << expression.isValid() << endl;
     cout << endl;
 
     // OR Elim
@@ -42,7 +69,7 @@ int main(int argc, char* argv[]){
     expression.addChild(FirstOrderTree("B"));
     expression.addChild(FirstOrderTree("~B"));
     expression.changeJustification("~I");
-    std::cout << "Result of NOT Intro Test 1: " << expression.isValid() << endl;
+    std::cout << "Result of NOT Introduction Rule Test #1: " << expression.isValid() << endl;
     cout << endl;
 
     expression = ProofChecker("~A");
@@ -50,7 +77,7 @@ int main(int argc, char* argv[]){
     expression.addChild(FirstOrderTree("A"));
     expression.addChild(FirstOrderTree("~B"));
     expression.changeJustification("~I");
-    std::cout << "Result of NOT Intro Test 2: " << expression.isValid() << endl;
+    std::cout << "Result of NOT Introduction Rule Test #2: " << expression.isValid() << endl;
     cout << endl;
 
     // Test NOT Elim
@@ -59,7 +86,7 @@ int main(int argc, char* argv[]){
     expression.addChild(FirstOrderTree("~B"));
     expression.addChild(FirstOrderTree("B"));
     expression.changeJustification("~E");
-    std::cout << "Result of NOT Elim Test 1: " << expression.isValid() << endl;
+    std::cout << "Result of NOT Elimination Rule Test #1: " << expression.isValid() << endl;
     cout << endl;
 
     expression = ProofChecker("C");
@@ -67,7 +94,7 @@ int main(int argc, char* argv[]){
     expression.addChild(FirstOrderTree("~C"));
     expression.addChild(FirstOrderTree("B"));
     expression.changeJustification("~E");
-    std::cout << "Result of NOT Elim Test 2: " << expression.isValid() << endl;
+    std::cout << "Result of NOT Elimination Rule Test #2: " << expression.isValid() << endl;
     cout << endl;
 
     // IF Intro cannot be tested yet, as we have not figured out how to represent
@@ -78,10 +105,10 @@ int main(int argc, char* argv[]){
     expression.addChild(FirstOrderTree("A"));
     expression.addChild(FirstOrderTree("A > B"));
     expression.changeJustification(">E");
-    std::cout << "Result of IF Elim (A), (A > B) => B: " << expression.isValid() << endl;
+    std::cout << "Result of IF Elimination Rule Test #1: " << expression.isValid() << endl;
     expression.addChild(FirstOrderTree("A > B"));
     expression.addChild(FirstOrderTree("A"));
-    std::cout << "Result of IF Elim (A > B), (A) => B: " << expression.isValid() << endl;
+    std::cout << "Result of IF Elimination Rule Test #2: " << expression.isValid() << endl;
     cout << endl;
 
     // Test IFF Intro
@@ -89,14 +116,14 @@ int main(int argc, char* argv[]){
     expression.addChild(FirstOrderTree("A > B"));
     expression.addChild(FirstOrderTree("B > A"));
     expression.changeJustification("=I");
-    std::cout << "Result of expression Intro (A > B, B > A) => A = B: " << expression.isValid() << endl;
+    std::cout << "Result of IFF Introduction Rule Test #1: " << expression.isValid() << endl;
     cout << endl;
 
     // Test IFF Elim
     expression = ProofChecker("A");
     expression.addChild(FirstOrderTree("A = B"));
     expression.addChild(FirstOrderTree("=E"));
-    std::cout << "Result of expression Intro (A = B, B) => A: " << expression.isValid() << endl;
+    std::cout << "Result of IFF Introduction Rule Test #2: " << expression.isValid() << endl;
     cout << endl;
 
     // Tests for quantifiers have not yet been written, as it has not been decided
